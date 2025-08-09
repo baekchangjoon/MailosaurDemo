2576444af7b49d68e38b8497a791526e1687aab7
baekchangjoon <changjoon.baek@icloud.com>
2025-08-09 07:36:33 -0700

    Create OtpControllerIT.java

---
+++ backend/src/test/java/demo/web/OtpControllerIT.java
@@ -0,0 +1,36 @@
+package demo.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.web.dto.SendOtpRequest;
+import demo.web.dto.VerifyOtpRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OtpControllerIT {
+  @Autowired MockMvc mvc;
+  @Autowired JavaMailSender sender;
+  @Autowired ObjectMapper om;
+
+  @Test void sendAndVerifyFlow() throws Exception {
+    doNothing().when(sender).send(any());
+    mvc.perform(post("/api/otp/send")
+      .contentType("application/json")
+      .content(om.writeValueAsString(new SendOtpRequest("t@e.st"))))
+      .andExpect(status().isAccepted());
+
+    mvc.perform(post("/api/otp/verify")
+      .contentType("application/json")
+      .content(om.writeValueAsString(new VerifyOtpRequest("t@e.st", "000000"))))
+      .andExpect(status().isBadRequest());
+  }
+}
