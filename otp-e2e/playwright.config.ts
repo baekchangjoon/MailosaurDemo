06721d827177b6016915973310a70bd92d78d9b0
baekchangjoon <changjoon.baek@icloud.com>
2025-08-10 02:19:05 +0900

    Apply slomo to playwright

--- otp-e2e/playwright.config.ts
+++ otp-e2e/playwright.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "@playwright/test";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
-  use: { baseURL: process.env.BASE_URL || "http://localhost:5173", headless: false },
+  use: {
+    baseURL: process.env.BASE_URL || "http://localhost:5173",
+    headless: false,
+    launchOptions: {
+      slowMo: 1000
+    }
+  },
   timeout: 60_000
 });
