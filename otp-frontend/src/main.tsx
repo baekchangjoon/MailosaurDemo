0195e207869f4af86a97c26d2ee790fd21944c83
baekchangjoon <changjoon.baek@icloud.com>
2025-08-09 07:40:09 -0700

    Create main.tsx

---
+++ otp-frontend/src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { LoginPage } from "../LoginPage";
+
+createRoot(document.getElementById("root")!).render(<LoginPage />);
