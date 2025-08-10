571815461d231e3b3789b2bdc8682ac5af3db1b7
baekchangjoon <changjoon.baek@icloud.com>
2025-08-09 20:39:10 -0700

    feat(totp): register TotpStore and TotpService beans (backend/src/main/java/demo/config/AppConfig.java)

--- backend/src/main/java/demo/config/AppConfig.java
+++ backend/src/main/java/demo/config/AppConfig.java
@@ -2,6 +2,8 @@ package demo.config;
 
 import demo.core.OtpService;
 import demo.core.OtpStore;
+import demo.core.TotpStore;
+import demo.core.TotpService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +19,15 @@ public class AppConfig {
   @Bean
   OtpService otpService(OtpStore store, JavaMailSender sender) {
     return new OtpService(store, sender);
+  }
+
+  @Bean
+  public TotpStore totpStore() {
+    return new TotpStore();
+  }
+
+  @Bean
+  public TotpService totpService(TotpStore s) {
+    return new TotpService(s, "MailosaurDemo");
   }
 }
